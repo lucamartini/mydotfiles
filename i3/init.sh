9fa2e99d790f2fa2a9bcad398b61aec36c2276f9
Luca Martini <luca.martini82@gmail.com>
2020-04-22 17:25:05 +0200

    add polybar

--- i3/init.sh
+++ i3/init.sh
@@ -5,8 +5,6 @@ xrandr --output DP-0 --primary --mode 1920x1080 --rate 84.90 --output DVI-I-1 --
 i3-msg 'workspace 1; exec firefox'
 i3-msg 'workspace 2; exec konsole'
 
-polybar --list-monitors | cut -d":" -f1 >> /tmp/poly.txt
-
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload example &
 done
