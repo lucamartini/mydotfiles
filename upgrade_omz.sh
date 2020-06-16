3a5d2c1ffa714731e820bd328b98cb4de9c1c0cd
Martini Luca <l.martini@nwgenergia.it>
2020-06-16 08:49:54 +0200

    add color

--- upgrade_omz.sh
+++ upgrade_omz.sh
@@ -1,8 +1,12 @@
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
 cd ~/.oh-my-zsh/custom/plugins/
 for dir in */     # list directories in the form "/tmp/dirname/"
 do
     dir=${dir%*/}      # remove the trailing "/"
 	cd $dir
+  echo "upgrading ${GREEN}$dir${NOCOLOR}"
 	git pull
 	cd ..
 done
@@ -12,6 +16,7 @@ for dir in */     # list directories in the form "/tmp/dirname/"
 do
     dir=${dir%*/}      # remove the trailing "/"
 	cd $dir
+  echo "upgrading ${GREEN}$dir${NOCOLOR}"
 	git pull
 	cd ..
 done
